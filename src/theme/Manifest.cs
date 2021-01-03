996e95c9fdfada83aef6f673efc74d62e9f5d26e
Luca Vicenzotti <l.vicenzotti@reply.it>
2021-01-03 18:13:38 +0100

    Initial scaffholding of Orchard Core projects

---
+++ src/theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "lucavice-tailwind-theme",
+    Author = "Luca Vicenzotti",
+    Website = "https://lucavice.com",
+    Version = "0.0.1",
+    Description = "lucavice-tailwind-theme"
+)]
