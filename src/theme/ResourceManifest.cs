b175a48440a50b7baf29a825677a1a0652fdc1c9
Luca Vicenzotti <l.vicenzotti@reply.it>
2021-01-15 19:18:11 +0100

    Implemented correct use of tag helpers

--- src/theme/ResourceManifest.cs
+++ src/theme/ResourceManifest.cs
@@ -9,10 +9,16 @@ namespace Lucavice.TailwindTheme
             var manifest = builder.Add();
 
             manifest
-                .DefineScript("lucavice-css")
-                .SetUrl("~/lucavice-tailwind-theme/assets/dist/css/lucavice.css", "~/lucavice-tailwind-theme/assets/dist/css/lucavice.css")
-                .SetVersion("1.0");
+                .DefineStyle("lucavice-css")
+                .SetUrl("~/Lucavice.TailwindTheme/assets/dist/css/lucavice.css", "~/Lucavice.TailwindTheme/assets/dist/css/lucavice.css");
 
+            manifest
+                .DefineStyle("highlight")
+                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/darcula.min.css"); 
+
+            manifest
+                .DefineStyle("merriweather-font")
+                .SetCdn("https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap");
         }
     }
 }
